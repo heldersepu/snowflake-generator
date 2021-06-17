e7f7e011ecf4a7d3681bfa285b23068f791248d8
Helder Sepulveda <heldersepu@gmail.com>
2021-06-17 12:59:03 -0400

    Hard code branches

--- snowflake-generator.js
+++ snowflake-generator.js
@@ -1,7 +1,6 @@
 let canvas = document.getElementById('snowflake'),
     ctx = canvas.getContext('2d'),
-    maxLevel = 5,
-    branches = 2;
+    maxLevel = 5;
 
 canvas.width = canvas.height = 600;
 ctx.translate(canvas.width / 2, canvas.height / 2);
@@ -13,7 +12,7 @@ for (let i = 0; i < 5; i++) {
     angles.push(Math.PI * 2 * Math.random());
 }
 
-let trunks = Math.floor(Math.random() * 3) + 5
+let trunks = Math.floor(Math.random() * 4) + 5
 for (let i = 0; i < trunks; i++) {
     drawLine(0);
     ctx.rotate(Math.PI * 2 / trunks);
@@ -29,10 +28,10 @@ function drawLine (level) {
     ctx.lineTo(200, 0);
     ctx.stroke();
 
-    for (let i = 1; i < branches + 1; i++) {
+    for (let i = 1; i < 3; i++) {
         ctx.save();
 
-        ctx.translate(200 * i / (branches + 1), 0);
+        ctx.translate(200 * i / 3, 0);
         ctx.scale(0.5, 0.5);
 
         ctx.save();
@@ -53,4 +52,4 @@ function drawLine (level) {
     }
 }
 
-drawLine(0);
+//drawLine(0);
